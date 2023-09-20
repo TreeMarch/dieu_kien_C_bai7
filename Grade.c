4b25ac2d8a33d1b57104f2146c2c08e16f67c4cb
Tran Trong Trinh <m5c0traan@gmail.com>
2023-09-20 21:26:08 +0700

    add news files

---
+++ Grade.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+//int Grade(){
+    int main(){
+        int marks1 ;
+        int marks2 ;
+
+    printf("enter marks1 number : ");
+    scanf("%d", marks1);
+    printf("enter marks2 number : ");
+    scanf("%d", marks2);
+
+    if(marks1 > 50 && marks2 < 50){
+        marks2  = marks2 + 5;
+        if  (marks2 >= 50){
+
+        }
+
+    }
+    }
+//}
